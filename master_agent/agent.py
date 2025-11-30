1ba7372f38dbc3fed29779d688ce2b7ede129a32
kpa1sai <korlapatipavansai@gmail.com>
2025-11-30 01:45:22 -0600

    Add human in the loop

--- master_agent/agent.py
+++ master_agent/agent.py
@@ -1,6 +1,4 @@
-from google.adk.agents.llm_agent import Agent
-from google.adk.agents.sequential_agent import SequentialAgent
-from google.adk.agents.loop_agent import LoopAgent
+from google.adk.agents.llm_agent import LlmAgent
 from ..procurement_agent.agent import root_agent as procurement_agent
 from ..negotitation_agent.agent import root_agent as negotiation_agent
 from google.adk.tools.function_tool import FunctionTool
@@ -14,9 +12,15 @@ async def external_approval_tool():
 
 approval_tool = FunctionTool(func=external_approval_tool)
 
-root_agent = SequentialAgent(
+root_agent = LlmAgent(
     name='root_agent',
     sub_agents=[procurement_agent, negotiation_agent],
     description='''A master agent that coordinates procurement and negotiation agents.''',
+    tools=[approval_tool],
+    instruction='''You are the master agent overseeing procurement and negotiation processes.
+    Your task is to delegate tasks to the procurement_agent and negotiation_agent as needed.
+    Use the external_approval_tool to get necessary approvals during the process.
+    Ensure smooth coordination between the two agents to achieve the best outcomes for procurement and negotiation tasks.
+    '''
 )
 
